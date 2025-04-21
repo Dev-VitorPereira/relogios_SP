91e345f37d510876cb3b17cde733fdaa1ac7e90a
Dev-VitorPereira <dev.vitorpereira@gmail.com>
2025-04-21 12:14:33 -0300

    feat: Criado Repository

--- src/main/java/com/relogios/relogios_SP/model/Relogio.java
+++ src/main/java/com/relogios/relogios_SP/model/Relogio.java
@@ -6,10 +6,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
 
 @Entity
 @Table
--- src/main/java/com/relogios/relogios_SP/repository/RelogioRepository.java
+++ src/main/java/com/relogios/relogios_SP/repository/RelogioRepository.java
@@ -1,4 +1,9 @@
 package com.relogios.relogios_SP.repository;
 
-public interface RelogioRepository {
+import com.relogios.relogios_SP.model.Relogio;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RelogioRepository extends JpaRepository<Relogio, Long> {
 }
