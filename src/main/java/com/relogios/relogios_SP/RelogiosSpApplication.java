b672e172a021d86664d7b5e8d46ae501ba3cadd6
Dev-VitorPereira <dev.vitorpereira@gmail.com>
2025-04-21 11:07:32 -0300

    feat: cirando projeto

---
+++ src/main/java/com/relogios/relogios_SP/RelogiosSpApplication.java
@@ -0,0 +1,13 @@
+package com.relogios.relogios_SP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelogiosSpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelogiosSpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/relogios/relogios_SP/RelogiosSpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.relogios.relogios_SP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelogiosSpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
