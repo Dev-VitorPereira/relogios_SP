e5d82fe79b1738162d5fd6afc06a5346e336cab7
Dev-VitorPereira <dev.vitorpereira@gmail.com>
2025-04-21 12:51:06 -0300

    feat: Criado controller

--- src/main/java/com/relogios/relogios_SP/controller/RelogioController.java
+++ src/main/java/com/relogios/relogios_SP/controller/RelogioController.java
@@ -1,4 +1,40 @@
 package com.relogios.relogios_SP.controller;
 
+import com.relogios.relogios_SP.DTO.RelogioDTO;
+import com.relogios.relogios_SP.service.RelogioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping ("/relogios")
+
 public class RelogioController {
+
+    @Autowired
+    private RelogioService relogioService;
+
+    @GetMapping
+    public List<RelogioDTO> listaTodos(){
+        return relogioService.listarTodos();
+    }
+    @GetMapping ("/{id}")
+    public RelogioDTO bucarPorId(@PathVariable Long id){
+        return relogioService.bucarPorId(id);
+    }
+
+    @PostMapping
+    public RelogioDTO salvar(@RequestBody RelogioDTO dto){
+        return relogioService.salvar(dto);
+    }
+    @PutMapping("/{id}")
+    public RelogioDTO atualizar(@PathVariable Long id, @RequestBody RelogioDTO dto) {
+        return relogioService.atualizar(id, dto);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletar(@PathVariable Long id) {
+        relogioService.deletar(id);
+    }
 }
--- src/main/java/com/relogios/relogios_SP/service/RelogioService.java
+++ src/main/java/com/relogios/relogios_SP/service/RelogioService.java
@@ -50,5 +50,4 @@ public class RelogioService {
         repository.deleteById(id);
     }
 
-
 }
