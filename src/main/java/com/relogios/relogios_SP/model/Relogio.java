69ce2aa9983ea38ee304edd358c62d7297629855
Dev-VitorPereira <dev.vitorpereira@gmail.com>
2025-04-21 11:42:15 -0300

    feat: criando entidades

--- src/main/java/com/relogios/relogios_SP/model/Relogio.java
+++ src/main/java/com/relogios/relogios_SP/model/Relogio.java
@@ -1,4 +1,26 @@
 package com.relogios.relogios_SP.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Entity
+@Table
 public class Relogio {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private int numberRelogio;
+    private String addressRelogio;
+    private double longitude;
+    private double latitude;
 }
